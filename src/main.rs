0773a994e57f5916adb6bb86e0759096e62cf1e8
oussama seffai <ousf.dev@gmail.com>
2024-05-01 16:51:01 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+fn main() {
+    println!("please guess a number between 1 and 10");
+    let winning_guess: i32 = rand::thread_rng().gen_range(1..10);
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("something went wrong");
+
+        let guess: i32 = guess.trim().parse().expect("not valid number");
+
+        match guess.cmp(&winning_guess) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big 'that is what she said'"),
+            Ordering::Equal => {
+                println!("You won");
+                break;
+            }
+        }
+    }
+}
